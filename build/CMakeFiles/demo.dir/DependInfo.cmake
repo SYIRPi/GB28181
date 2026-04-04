0b893c320bef7b117c8d513101b64ab275b60819
SYIRPi <SYIRPi@163.com>
2026-04-04 12:25:13 +0800

    summit

---
+++ build/CMakeFiles/demo.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/BaiduNetdiskDownload/项目2-26/project2/config.cc" "CMakeFiles/demo.dir/config.cc.obj" "gcc" "CMakeFiles/demo.dir/config.cc.obj.d"
+  "D:/BaiduNetdiskDownload/项目2-26/project2/decode.cc" "CMakeFiles/demo.dir/decode.cc.obj" "gcc" "CMakeFiles/demo.dir/decode.cc.obj.d"
+  "D:/BaiduNetdiskDownload/项目2-26/project2/gb28181.cc" "CMakeFiles/demo.dir/gb28181.cc.obj" "gcc" "CMakeFiles/demo.dir/gb28181.cc.obj.d"
+  "D:/BaiduNetdiskDownload/项目2-26/project2/main.cc" "CMakeFiles/demo.dir/main.cc.obj" "gcc" "CMakeFiles/demo.dir/main.cc.obj.d"
+  "D:/BaiduNetdiskDownload/项目2-26/project2/ps_rtp.cc" "CMakeFiles/demo.dir/ps_rtp.cc.obj" "gcc" "CMakeFiles/demo.dir/ps_rtp.cc.obj.d"
+  "D:/BaiduNetdiskDownload/项目2-26/project2/push.cc" "CMakeFiles/demo.dir/push.cc.obj" "gcc" "CMakeFiles/demo.dir/push.cc.obj.d"
+  "D:/BaiduNetdiskDownload/项目2-26/project2/xml_utils.cc" "CMakeFiles/demo.dir/xml_utils.cc.obj" "gcc" "CMakeFiles/demo.dir/xml_utils.cc.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
